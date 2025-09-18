15672e2f57fb60b0ca49b34646ba6a34d5f11240
JustinHeath <44195109+MeanTemperature@users.noreply.github.com>
2025-09-18 20:35:22 +0100

    bits added to line 2 and a new line 5

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log('Hello main, from feat/new-idea');
-console.log('I am a feature branch');
+console.log('I am a feature branch');                                               // just an idea-3 bit.
 console.log('I am another stupid edit from another branch - feat/new-idea');
 console.log('Yet another stupid edit from feat/new-idea');
+// not so much an idea as a comment from feat/idea-3
